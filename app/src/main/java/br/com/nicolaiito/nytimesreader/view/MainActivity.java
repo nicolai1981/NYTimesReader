6db748b0b3d2689eff824a02c572bee857f19aae
nicolai ito <nicolai.ito@sonymobile.com>
2017-03-27 19:36:59 -0300

    Add about dialog

--- app/src/main/java/br/com/nicolaiito/nytimesreader/view/MainActivity.java
+++ app/src/main/java/br/com/nicolaiito/nytimesreader/view/MainActivity.java
@@ -1,16 +1,27 @@
 package br.com.nicolaiito.nytimesreader.view;
 
+import android.app.Dialog;
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
@@ -24,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     private SearchView mSearchView;
     private LinearLayoutManager mLayoutManager;
     private int mCurrentLastPosition;
+    private Dialog mDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,9 +78,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onPause() {
+        if (mDialog != null) {
+            mDialog.dismiss();
+            mDialog = null;
+        }
+        super.onPause();
+    }
+
+    @Override
     public void onStop() {
-        super.onStop();
         EventBus.getDefault().unregister(this);
+        super.onStop();
     }
 
     @Override
@@ -106,17 +127,52 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_search) {
-            return true;
+        if (item.getItemId() == R.id.action_about) {
+            createDialog();
         }
         return super.onOptionsItemSelected(item);
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onMessageEvent(ArticleViewModel.ListUpdatedEvent event) {
+        // TODO add a better way to avoid the scroll when the list is updated
         if (mLayoutManager.getItemCount() > mCurrentLastPosition) {
             mLayoutManager.scrollToPosition(mCurrentLastPosition);
         }
+    }
+
+    private void createDialog() {
+        if (mDialog != null) {
+            mDialog.dismiss();
+            mDialog = null;
+        }
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_about, null);
+        TextView txtMsg = (TextView) dialogView.findViewById(R.id.license_content);
+        txtMsg.setText(Html.fromHtml(getResources().getString(R.string.license_text)));
+        txtMsg.setMovementMethod(LinkMovementMethod.getInstance());
+
+        dialogView.findViewById(R.id.api_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://developer.nytimes.com"));
+                startActivity(intent);
+            }
+        });
+
+        dialogBuilder.setTitle(R.string.dialog_title_license);
+        dialogBuilder.setView(dialogView);
+        dialogBuilder.setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                mDialog.dismiss();
+                mDialog = null;
+            }
+        });
+        mDialog = dialogBuilder.create();
+        mDialog.show();
     }
 }
